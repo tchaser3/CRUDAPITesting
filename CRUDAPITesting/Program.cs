384abd6743d4d2319f8cadae1a115e3526440bb7
Terry Holmes <tchaser3@outlook.com>
2024-04-11 06:25:42 -0400

    Add project files.

---
+++ CRUDAPITesting/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NewEventLogDLL;
+
+namespace CRUDAPITesting
+{ 
+    
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+    internal class Program
+    {
+        static EventLogClass TheEventLogClass = new EventLogClass();
+
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name: {product.Name}\tPrice: " + $"{product.Price}\tCategory: {product.Category}");
+        }
+
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/products", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        static async Task<Product> UpdateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Product>();
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"api/products/{id}");
+            return response.StatusCode;
+        }
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Create a new product
+                Product product = new Product
+                {
+                    Name = "Gizmo",
+                    Price = 100,
+                    Category = "Widgets"
+                };
+
+                var url = await CreateProductAsync(product);
+                Console.WriteLine($"Created at {url}");
+
+                // Get the product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Update the product
+                Console.WriteLine("Updating price...");
+                product.Price = 80;
+                await UpdateProductAsync(product);
+
+                // Get the updated product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(product.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "CRUD API Testing " + e.ToString());
+
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
